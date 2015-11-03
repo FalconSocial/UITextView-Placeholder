a94d40f191f660277a45865219a9eafc20169e04
Tim Studt <timstudt@gmail.com>
2015-11-03 16:15:07 +0100

    label: init font to match textView.font

--- UITextView+Placeholder/UITextView+Placeholder.h
+++ UITextView+Placeholder/UITextView+Placeholder.h
@@ -28,7 +28,7 @@
 
 @property (nonatomic, readonly) UILabel *placeholderLabel;
 
-@property (nonatomic, strong) NSString *placeholder;
+@property (nonatomic, strong) IBInspectable NSString *placeholder;
 @property (nonatomic, strong) NSAttributedString *attributedPlaceholder;
 @property (nonatomic, strong) UIColor *placeholderColor;
 
